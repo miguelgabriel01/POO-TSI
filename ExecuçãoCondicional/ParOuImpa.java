85484321fdc49e96da3693e000cda9d15c78c5f7
mitguelgabriel01 <mgbs@discente.ifpe.edu.br>
2022-03-15 00:55:52 -0300

    Verificar se o numero é par ou impar

---
+++ ExecuçãoCondicional/ParOuImpa.java
@@ -0,0 +1,14 @@
+public class ParOuImpa{
+   
+    //programa que define se o numero é par ou impar
+    public static void main(String[] args){
+
+        int numero = 9;
+
+        if(numero %2==0){
+            System.out.println("O numero e par");
+        }else{
+            System.out.println("Numero impar");
+        }
+    }
+}
