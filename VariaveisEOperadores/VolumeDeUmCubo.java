a6530220d52efb499fe5a5c388c5ffb45b7d75b4
mitguelgabriel01 <mgbs@discente.ifpe.edu.br>
2022-03-15 00:41:37 -0300

    Volume de um cubo

---
+++ VariaveisEOperadores/VolumeDeUmCubo.java
@@ -0,0 +1,11 @@
+public class VolumeDeUmCubo{
+
+    //Volume de um cubo,aresta ao cubo
+    public static void main(String[] args){
+        int aresta = 10;
+
+        int resposta;
+        resposta = aresta * aresta;
+        System.out.println("Volume do cubo: " + resposta);
+    }
+}
