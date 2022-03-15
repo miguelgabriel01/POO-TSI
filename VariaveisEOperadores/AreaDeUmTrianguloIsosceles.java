07a564525b17f40e7f12e7b3f243da62aeaf0ea9
mitguelgabriel01 <mgbs@discente.ifpe.edu.br>
2022-03-15 00:36:50 -0300

    Area de um triangulo isosceles

---
+++ VariaveisEOperadores/AreaDeUmTrianguloIsosceles.java
@@ -0,0 +1,13 @@
+public class AreaDeUmTrianguloIsosceles {
+
+    //Area de um triangulo isósceles, base vezes altura dividido por dois
+    public static void main(String[] args){
+        float base = 1.55f;
+        float altura = 3.85f;
+        float resposta;
+
+        resposta = base * altura / 2;
+        System.out.println("Area e igual a: " + resposta);
+
+    }
+}
