3d032cd4b7672ca16bebae6256b7ea3be7410f16
mitguelgabriel01 <mgbs@discente.ifpe.edu.br>
2022-03-15 00:47:06 -0300

    Media de seis numeros

---
+++ VariaveisEOperadores/MediaSeisNumeros.java
@@ -0,0 +1,17 @@
+public class MediaSeisNumeros{
+    
+    //Media de seis numeros
+    public static void main(String[] args){
+        float notaA = 6.5f;
+        float notaB = 7.3f;
+        float notaC = 10.0f;
+        float notaD = 4.6f;
+        float notaE = 8.6f; 
+        float notaF = 2.4f;
+
+        float media;
+        media = (notaA + notaB + notaC + notaD + notaE + notaF)/6;
+
+        System.out.println("Media: " + media);
+    }
+}
