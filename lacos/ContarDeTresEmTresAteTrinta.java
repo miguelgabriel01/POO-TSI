2f00bbcddfffb61c54a706178d65aebca7a5aea9
mitguelgabriel01 <mgbs@discente.ifpe.edu.br>
2022-03-22 00:03:35 -0300

    Contar de 3 em 3 so 0 ate 30

---
+++ lacos/ContarDeTresEmTresAteTrinta.java
@@ -0,0 +1,7 @@
+public class ContarDeTresEmTresAteTrinta {
+    public static void main(String[] args){
+        for(int contador = 0; contador<33; contador = contador+3){
+            System.out.println(contador);
+        }
+    }
+}
