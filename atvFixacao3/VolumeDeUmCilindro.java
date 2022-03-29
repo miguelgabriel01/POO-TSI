caa6a3a5caa1da7683e98c3e5b4627ce510a7a7e
mitguelgabriel01 <mgbs@discente.ifpe.edu.br>
2022-03-28 23:09:05 -0300

    volume de um cilindro

---
+++ atvFixacao3/VolumeDeUmCilindro.java
@@ -0,0 +1,14 @@
+public class VolumeDeUmCilindro {
+
+    //Exercicio - variaveis e operadores
+    //slide pag 10, exercicio 10
+
+    public static void main(String[] args){
+        float areaDaBase = 10.7f;
+        float altura = 12.5f;
+
+        float calculo = areaDaBase *altura;
+
+        System.out.println("Volume de um cilindro: " + calculo);
+    }
+}
