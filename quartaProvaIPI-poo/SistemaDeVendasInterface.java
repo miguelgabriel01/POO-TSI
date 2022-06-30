cecb96bfd797d8e4e041618203310b32ab963250
miguelgabriel01 <gabrielogabriel10@gmail.com>
2022-06-30 18:25:01 -0300

    Criação da interface responsavel pela venda do veiculo

---
+++ quartaProvaIPI-poo/SistemaDeVendasInterface.java
@@ -0,0 +1,5 @@
+public interface SistemaDeVendasInterface {
+    public boolean verificarSeNovoVeiculoEstaDisponivel();
+    public boolean getAcionarClienteSobreVeiculo();
+    public boolean getRespostaDoClienteSobreOVeiculo();
+}
