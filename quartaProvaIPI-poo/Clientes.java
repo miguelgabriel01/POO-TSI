386f935d87272b6c045df4272ee8ccde95c83c1a
miguelgabriel01 <gabrielogabriel10@gmail.com>
2022-06-30 18:11:59 -0300

    Criação da class Clientes

--- quartaProvaIPI-poo/Clientes.java
+++ quartaProvaIPI-poo/Clientes.java
@@ -2,4 +2,10 @@ public class Clientes extends DadosPessoaisBasicos{
     public Clientes(String nomeCompleto, String cpf, String endereco, String telefoneDeContato, String email){
         super(nomeCompleto,cpf,endereco,telefoneDeContato,email);
     }
+
+        //instanciamos a class
+        public static Funcionarios dadosClientes(){
+            Clientes clientesDadosBasicos = new Clientes("paulo jose da silva", "44455588827", "Rua sem saida", "81554785854", "meajuda@poo.com.br");
+            return clientesDadosBasicos;
+        }
 }
